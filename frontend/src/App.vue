c51e28e9ee57b21fc7a9d05ecadbee0a935d0c68
rorikoron <sub.ac.000000001@gmail.com>
2025-04-22 02:42:33 +0800

    update: make toolbar drawable

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,52 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import Toolbar from "./layouts/ToolBar.vue";
+import { useQuasar } from "quasar";
+import { usePocketbaseStore } from "./stores/pocketbase";
+import ProposeForm from "@/components/ProposeForm.vue";
+const $q = useQuasar();
+const { refreshProposes } = usePocketbaseStore();
+
+const drawer = ref(true);
+const miniState = ref(false);
+const createProposeForm = () => {
+  $q.dialog({ component: ProposeForm }).onOk(() => {
+    refreshProposes();
+    $q.notify({ message: "成功發起提議", position: "bottom-right" });
+  });
+};
+// <nav :class="$q.dark.isActive ? 'bg-dark' : 'bg-inactive'">
 </script>
 
 <template>
   <q-layout class="row">
-    <Toolbar class="col-2" />
+    <q-drawer
+      v-model="drawer"
+      show-if-above
+      :mini="miniState"
+      @mouseenter="miniState = false"
+      @mouseleave="miniState = true"
+      :width="200"
+      :breakpoint="500"
+      bordered
+    >
+      <Toolbar class="col-2" />
+    </q-drawer>
     <q-page-container class="col-grow">
       <q-page padding>
         <router-view />
       </q-page>
+
+      <q-page-sticky position="bottom-right">
+        <q-btn
+          flat
+          rounded
+          icon="fa-solid fa-plus"
+          size="md"
+          label="新增共乘提議"
+          @click="createProposeForm"
+        />
+      </q-page-sticky>
     </q-page-container>
   </q-layout>
 </template>
--- frontend/src/layouts/ToolBar.vue
+++ frontend/src/layouts/ToolBar.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 import { useQuasar } from "quasar";
-import ProposeForm from "../components/ProposeForm.vue";
-import { usePocketbaseStore } from "../stores/pocketbase";
 
 const $q = useQuasar();
-const { refreshProposes } = usePocketbaseStore();
 const links = [
   {
     icon: "fa-solid fa-users",
@@ -12,58 +9,34 @@ const links = [
     to: "/proposes",
   },
 ];
-
-const createProposeForm = () => {
-  $q.dialog({ component: ProposeForm }).onOk(() => {
-    refreshProposes();
-    $q.notify({ message: "成功發起提議", position: "bottom-right" });
-  });
-};
-
-const postProposeForm = () => {
-  try {
-  } catch (e) {
-    console.log(e);
-  }
-};
 </script>
 
 <template>
-  <nav :class="$q.dark.isActive ? 'bg-dark' : 'bg-inactive'">
-    <q-list class="q-pa-sm">
-      <q-item
-        v-for="(link, index) in links"
-        :key="index"
-        :to="link.to"
-        clickable
-        v-ripple
-        class="items-center"
-      >
-        <q-item-section avatar>
-          <q-avatar :icon="link.icon" size="2em" class="q-mr-sm" />
-        </q-item-section>
-        <q-item-label class="text-h6">{{ link.text }}</q-item-label>
-      </q-item>
-      <q-btn
-        flat
-        rounded
-        icon="fa-solid fa-plus"
-        size="md"
-        label="新增共乘提議"
-        @click="createProposeForm"
-      />
-    </q-list>
+  <q-list padding>
+    <q-item
+      v-for="(link, index) in links"
+      :key="index"
+      :to="link.to"
+      clickable
+      v-ripple
+      class="items-center"
+    >
+      <q-item-section avatar>
+        <q-avatar :icon="link.icon" size="2em" />
+      </q-item-section>
+      <q-item-section class="text-h6">{{ link.text }}</q-item-section>
+    </q-item>
+  </q-list>
 
-    <!-- theme changer -->
-    <q-list class="q-pa-sm">
-      <q-item class="items-center">
-        <q-icon name="fa-solid fa-sun" v-if="!$q.dark.isActive" />
-        <q-icon name="fa-solid fa-moon" v-else />
-        <q-toggle
-          :model-value="$q.dark.isActive"
-          @update:model-value="$q.dark.toggle"
-        />
-      </q-item>
-    </q-list>
-  </nav>
+  <!-- theme changer -->
+  <q-list class="q-pa-sm">
+    <q-item class="items-center">
+      <q-icon name="fa-solid fa-sun" v-if="!$q.dark.isActive" />
+      <q-icon name="fa-solid fa-moon" v-else />
+      <q-toggle
+        :model-value="$q.dark.isActive"
+        @update:model-value="$q.dark.toggle"
+      />
+    </q-item>
+  </q-list>
 </template>
