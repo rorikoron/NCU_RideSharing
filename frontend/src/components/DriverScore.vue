6534aa2c6b01388865deb000d4e3b4d53cea65d8
PHao <pinhao940629@gmail.com>
2025-04-21 15:09:20 +0800

    新增評分顯示

---
+++ frontend/src/components/DriverScore.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  score: number;
+}>();
+
+const ceildScore = Math.round(props.score);
+</script>
+
+<template>
+  <div class="row items-center q-gutter-sm">
+    <q-rating
+      :model-value="ceildScore"
+      max="5"
+      color="yellow"
+      icon="star_border"
+      size="md"
+      icon-selected="star"
+      readonly
+    />
+    <span>{{ score }}</span>
+  </div>
+</template>
