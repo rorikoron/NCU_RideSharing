2b8c54c665786a1a4b9afbe4a86b30981e8a51bd
destine940816@gmail.com <destine940816@gmail.com>
2025-05-09 16:20:41 +0800

    remove unused interface

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
 import { onBeforeMount, ref } from "vue";
 import Toolbar from "./layouts/ToolBar.vue";
 import { useQuasar } from "quasar";
-import { usePocketbaseStore, type User } from "./stores/pocketbase";
+import { usePocketbaseStore } from "./stores/pocketbase";
 import ProposeForm from "@/components/ProposeForm.vue";
 import { useIdentity } from "./stores/identity";
 const $q = useQuasar();
